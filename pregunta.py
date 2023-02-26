195727bc7069e4850dc99d4b3365f32f91487319
Johan-Madronero-Cuervo <jmadronero@unal.edu.co>
2023-02-25 23:52:23 -0500

    starting

--- pregunta.py
+++ pregunta.py
@@ -10,11 +10,29 @@ import pandas as pd
 
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
     #
     # Inserte su código aquí
     #
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+
+    df.sexo = df.sexo.str.lower()
+
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+
+    df.idea_negocio = df.idea_negocio.str.lower()
+
+    df.barrio = df.barrio.str.lower()
+
+    df.estrato = df.estrato.astype(int)
+
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace('$',''))
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace(',',''))
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace('.00',''))
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+
+    df.línea_credito = df.línea_credito.str.lower()
 
     return df
